91ecc66511e7b0cc630112ccf2e8f92c4d627140
inoxybel <inoxybel@live.com>
2024-11-22 22:13:32 -0300

    refact: contact model

--- Contact/Domain/Entities/Contact.cs
+++ Contact/Domain/Entities/Contact.cs
@@ -2,7 +2,7 @@
 
 public class Contact
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Id { get; set; } = string.Empty;
     public required string Name { get; set; }
     public required short AreaCode { get; set; }
     public required long Phone { get; set; }
@@ -11,4 +11,10 @@ public class Contact
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public bool IsEnabled { get; set; }
+
+    public Contact()
+    {
+        Id = Guid.NewGuid().ToString();
+        CreatedAt = DateTime.UtcNow;
+    }
 }
